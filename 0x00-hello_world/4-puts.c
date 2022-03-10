f7440352e70134cd4616eac54471ab9d16ac5eb0
Fiona Glory <fionasonny132@gmail.com>
2022-03-10 08:43:32 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/*
+*function that print ou a string and returns 0
+*/
 int main()
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
