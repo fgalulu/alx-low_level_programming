bcbddb83ad65d1cee4dc8ea795a223bcb6b1d703
fgalulu <fionasonny132@gmail.com>
2022-03-11 12:40:07 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print number from 0 to 9
+ * end with a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
