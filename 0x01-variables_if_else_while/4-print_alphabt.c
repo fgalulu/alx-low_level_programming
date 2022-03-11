5383483f3f3b9fbdfc69b9f3374357337d839b26
fgalulu <fionasonny132@gmail.com>
2022-03-11 12:01:09 +0000

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,13 @@ int main(void)
 {
 	char i = 'a';
 
-	while ((i <= 'z') && (i != 'e') && (i != 'q'))
+	while (i <= 'z')
 	{
-		putchar(i);
-		i++;
+		if ((i != 'e') || (i != 'q'))
+		{
+			putchar(i);
+			i++;
+		}
 	}
 
 	putchar('\n');
