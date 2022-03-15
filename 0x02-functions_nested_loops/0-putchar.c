91a716a67549ffa53d94fbadfae37ac312daa040
fgalulu <fionasonny132@gmail.com>
2022-03-15 12:01:23 +0000

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -main function
+ * Description: function uses _putchar function to print a string
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+
+	}
+	_putchar('\n');
+	return (0);
+}
