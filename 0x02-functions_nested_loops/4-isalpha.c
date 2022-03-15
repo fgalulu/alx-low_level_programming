93f34085b6ef7f98286a6f13a189fb6894268c97
fgalulu <fionasonny132@gmail.com>
2022-03-15 13:35:28 +0000

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - checks if parameter is an alphabet
+ * @c: chater to be converted to ASCII
+ *
+ * Return: 1 if condition is met. otherwise 0
+ */
+int _isalpha(int c);
+{
+	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
+}
