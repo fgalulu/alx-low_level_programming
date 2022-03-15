2b1e00023777373006249a201cd5a16adfb62028
fgalulu <fionasonny132@gmail.com>
2022-03-15 17:24:32 +0000

    task 7 fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,11 @@ int print_last_digit(int n)
 {
 	int last;
 
-	n = n >= 0 ? n : n * -1;
-	last = n % 10;
-
+	if (n < 0)
+		last = -1 * (n % 10);
+	else
+		last = n % 10;
+	
 	_putchar('0' + last);
-
 	return (last);
 }
