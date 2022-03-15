c7347941aecef4eec244f007a3c0f0be717de832
fgalulu <fionasonny132@gmail.com>
2022-03-15 13:53:27 +0000

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - funtion that check the valueof number
+ * @n: parameter to be checked
+ *
+ * Return: 1 if n is greater than 0, 0 if n is zero and -1 otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
