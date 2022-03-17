3f11877e04a521cf55e1861ab168216fa9b128da
fgalulu <fionasonny132@gmail.com>
2022-03-17 12:12:57 +0000

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiples two number given
+ * @a: first integer parameter
+ * @b: seconf integer parameter
+ * Return: integer result
+ */
+int mul(int a, int b)
+{
+	return ( a * b);
+}
