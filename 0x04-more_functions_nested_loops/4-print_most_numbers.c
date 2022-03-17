09da51b6ba4a966ecdb27e37fd356cb8c3e6b657
fgalulu <fionasonny132@gmail.com>
2022-03-17 13:04:13 +0000

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers except 2
+ * and 4 followed by new line
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('\n');
+}
