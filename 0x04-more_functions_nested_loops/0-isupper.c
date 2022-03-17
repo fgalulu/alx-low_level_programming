f235ab6b280d27b9fbddc563d80c31d8ba87ba72
fgalulu <fionasonny132@gmail.com>
2022-03-17 11:56:13 +0000

    task 0 fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,9 @@
  *  _isupper - checks if the character is uppercase
  *  @c: character to be checked
  *
- *  Return: 0 or 1 
+ *  Return: 0 or 1
  */
 int _isupper(int c)
 {
-	if ( c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c > 64 && c < 91);
 }
